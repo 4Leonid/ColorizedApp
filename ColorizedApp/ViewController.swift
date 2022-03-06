f9d8352542bfb277262a6e6f51edc32ddc951f1b
4Leonid <leo-turko@mail.ru>
2022-03-06 15:06:40 +0300

    Logic settings

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redNumberLabel: UILabel!
+    @IBOutlet var greenNumberLabel: UILabel!
+    @IBOutlet var blueNumberLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        coloredView.layer.cornerRadius = 16
+        
+        redSlider.configure(tintColor: .red)
+        greenSlider.configure(tintColor: .green)
+        blueSlider.configure(tintColor: .blue)
+        
+        redLabel.configure()
+        greenLabel.configure()
+        blueLabel.configure()
+        
+        redNumberLabel.configure(sliderValue: redSlider.value)
+        greenNumberLabel.configure(sliderValue: greenSlider.value)
+        blueNumberLabel.configure(sliderValue: blueSlider.value)
+        
+        setbackGroundColore()
     }
-
-
+    
+    @IBAction func sliderAction() {
+        
+        redNumberLabel.configure(sliderValue: redSlider.value)
+        greenNumberLabel.configure(sliderValue: greenSlider.value)
+        blueNumberLabel.configure(sliderValue: blueSlider.value)
+        
+        setbackGroundColore()
+    }
+    
+    private  func setbackGroundColore() {
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
+extension UISlider {
+    func configure(tintColor: UIColor? = nil) {
+        value = Float.random(in: 0...1)
+        minimumTrackTintColor = tintColor
+        minimumValue = 0
+        maximumValue = 1
+    }
+}
+
+extension UILabel {
+    func configure(sliderValue: Float? = nil) {
+        textColor = .white
+        guard let sliderValue = sliderValue else { return }
+        text = String(format: "%.2f", sliderValue)
+    }
+}
